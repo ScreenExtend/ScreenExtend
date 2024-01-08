5a33ad4858990f8cacf1a12cf79df5d6b613b2a5
EpicCodeWizard <83260377+EpicCodeWizard@users.noreply.github.com>
2024-01-08 08:33:10 +0100

    chore: feedback fix

--- src/components/pages/register-auth-form.tsx
+++ src/components/pages/register-auth-form.tsx
@@ -11,12 +11,15 @@ import {
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
 
 const formSchema = z.object({
   email: z.string().email({ message: "Please enter a valid email" }),
   password: z.string().min(8, { message: "Please enter a valid password" }),
 });
 export function RegisterAuthForm() {
+  const [showPassword, setShowPassword] = useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -28,6 +31,10 @@ export function RegisterAuthForm() {
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
+  }
+
+  function togglePasswordVisibility() {
+    setShowPassword((prev) => !prev);
   }
 
   return (
@@ -53,7 +60,27 @@ export function RegisterAuthForm() {
             <FormItem className="space-y-0 text-left">
               <FormLabel>Password</FormLabel>
               <FormControl>
-                <Input type="password" placeholder="********" {...field} />
+                <div className="relative outline-none">
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                    className="outline-none"
+                    {...field}
+                  />
+                  <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 cursor-pointer">
+                    {showPassword ? (
+                      <EyeOff
+                        className="h-5 w-5"
+                        onClick={togglePasswordVisibility}
+                      />
+                    ) : (
+                      <Eye
+                        className="h-5 w-5"
+                        onClick={togglePasswordVisibility}
+                      />
+                    )}
+                  </div>
+                </div>
               </FormControl>
               <FormMessage />
             </FormItem>
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -4,9 +4,17 @@ import { Switch } from "@/components/ui/switch";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 export default function Settings() {
-  const [isPublic, setIsPublic] = useState(true);
+  const [isPrivate, setIsPrivate] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  function togglePasswordVisibility() {
+    if (!isPrivate) return;
+    setShowPassword((prev) => !prev);
+  }
 
   return (
     <Layout>
@@ -23,18 +31,44 @@ export default function Settings() {
               <div className=" flex items-center space-x-4 border-b p-3 px-0">
                 <div className="flex-1 space-y-1">
                   <p className="text-sm font-medium leading-none">
-                    Toggle Public
+                    Toggle Private
                   </p>
                 </div>
-                <Switch checked={isPublic} onCheckedChange={setIsPublic} />
+                <Switch checked={isPrivate} onCheckedChange={setIsPrivate} />
               </div>
               <div className="flex items-center space-x-4 p-3 px-0">
-                <Input
+                {/* <Input
                   type="password"
                   placeholder="Password"
-                  disabled={isPublic}
-                />
-                <Button disabled={isPublic}>Save Password</Button>
+                  disabled={!isPrivate}
+                /> */}
+                <div className="relative outline-none flex-1">
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                    className="outline-none"
+                    disabled={!isPrivate}
+                  />
+                  <div
+                    className={cn(
+                      "absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 cursor-pointer",
+                      !isPrivate && "cursor-not-allowed"
+                    )}
+                  >
+                    {showPassword ? (
+                      <EyeOff
+                        className="h-5 w-5"
+                        onClick={togglePasswordVisibility}
+                      />
+                    ) : (
+                      <Eye
+                        className="h-5 w-5"
+                        onClick={togglePasswordVisibility}
+                      />
+                    )}
+                  </div>
+                </div>
+                <Button disabled={!isPrivate}>Save Password</Button>
               </div>
             </CardContent>
           </Card>
