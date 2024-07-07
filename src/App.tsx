4efc4d2263b01a944156171cf295e1860288a497
Sarvesh Madullapalli <83260377+EpicCodeWizard@users.noreply.github.com>
2024-07-07 14:21:36 -0700

    chore: remove unused window attribute declaration

--- src/App.tsx
+++ src/App.tsx
@@ -87,13 +87,6 @@ console.log(await invoke("start_hosted_network", {ssid: `ScreenExtend${Array.fro
 Get Theme Without State:
 ((theme === "system") ? ((window.document.documentElement.classList.toString() === "light") ? ("LIGHT") : ("DARK")) : (theme === "light") ? ("LIGHT") : ("DARK"))
 
-Add Attribute to Window (add to auth-provider.tsx):
-declare global {
-    interface Window {
-        changeSidebarState: () => void;
-    }
-}
-
 Library Order:
 react
 react-router-dom
