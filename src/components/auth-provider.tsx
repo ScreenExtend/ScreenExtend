dc4d381ba3c56328b1d34f82ef5497f14c60120c
EpicCodeWizard <83260377+EpicCodeWizard@users.noreply.github.com>
2024-07-07 14:21:36 -0700

    Changes

--- src/components/auth-provider.tsx
+++ src/components/auth-provider.tsx
@@ -3,4 +3,8 @@ type AuthContextType = {
     currentUser: { username: string, password: string },
     setCurrentUser: React.Dispatch<React.SetStateAction<{ username: string, password: string }>>
 };
+declare global {
+    interface Window {
+    }
+}
 export const AuthProviderContext = createContext<AuthContextType>({ currentUser: { username: "", password: "" }, setCurrentUser: () => {} });
